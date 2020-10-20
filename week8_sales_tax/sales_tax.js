32bd899e0de4fcf271fb8636ae6e8b73e278a418
abulsufiyan12 <72047362+abulsufiyan12@users.noreply.github.com>
2020-10-20 07:00:15 +0530

    Add files via upload

---
+++ week8_sales_tax/sales_tax.js
@@ -0,0 +1,46 @@
+var $ = function (id) {
+    return document.getElementById(id); 
+};
+
+function init() {
+    $("subtotal").focus();
+    $("clear").onclick = clear;
+    $("subtotal").onclick = function() {
+        $("subtotal").value = "";
+    };
+    $("tax_rate").onclick = function() {
+        $("tax_rate").value = "";
+    }
+}
+
+function processEntries() {
+    
+    //Get inputs
+    var sub_total = parseFloat($("subtotal").value);
+    var tax_rate = parseFloat($("tax_rate").value);
+    
+    //Validate inputs
+    if(sub_total < 0 || sub_total > 10000 || tax_rate < 0 || tax_rate > 12){
+        alert("Subtotal must be > 0 and < 10000 \n Tax rate must be >0 and <12");
+        $("subtotal").value = "";
+        $("tax_rate").value = "";
+        $("subtotal").focus();
+        return;
+    }
+    
+    //Calculation
+    var sales_tax = sub_total*tax_rate/100;
+    var total = sub_total + sales_tax;
+    //Display the result
+    $("sales_tax").value = sales_tax;
+    $("total").value = total;
+    $("subtotal").focus()
+}
+
+var clear = function() {
+    $("subtotal").value = "";
+    $("tax_rate").value = "";
+    $("subtotal").focus();
+
+}
+
